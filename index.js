0e73d25a9838ca5710060e3a4bb967be89f86873
queenanemone <shinhs9902@gmail.com>
2022-10-15 17:27:24 +0900

    ADD : 파이어베이스와 node.js 서버 연동(데이터 추가, 삭제 구현)

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const portNum = 3000;
+const { db } = require("./firebase.js"); // firebase.js 임포트
 
-app.get('/', (req, res) => {
-  res.send('Hello world!!');
+app.use(express.json());
+
+const friends = {
+  // 예시 자료
+  james: "friend",
+  larry: "friend",
+  lucy: "friend",
+  banana: "enemy",
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello world!!");
+});
+
+app.post("/dataAdd", async (req, res) => {
+  // 데이터의 추가
+  const { name, status } = req.body;
+  console.log(req.body, name, status);
+  await db
+    .collection("people")
+    .doc("Person1") // 나중에 이 부분을 user의 UID로 바꿀 것
+    .set({
+      // 대괄호를 사용, 데이터를 집어넣는다
+      [name]: status,
+    });
+  res.status(200).send(); // status(200), 성공 상태를 전송
+});
+
+app.delete("/dataDel", async (req, res) => {
+  // 데이터의 삭제
+  const { name, status } = req.body;
+  console.log(req.body, name, status);
+  await db
+    .collection("people")
+    .doc("Person1") // 나중에 이 부분을 user의 UID로 바꿀 것
+    .delete(); // delete 함수
+  res.status(200).send(); // status(200), 성공 상태를 전송
 });
 
 app.listen(portNum, () => {
-  console.log('서버가 실행중입니다...');
+  console.log("서버가 실행중입니다...");
 });
